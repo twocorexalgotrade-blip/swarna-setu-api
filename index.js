1b5faeee5d461abdb731f16b92b2c57f9776c6d4
twocorexalgotrade-blip <twocorexalgotrade@gmail.com>
2026-01-12 12:54:35 +0530

    feat(game)

--- index.js
+++ index.js
@@ -170,7 +170,7 @@ const createVendorCredentialsTable = async () => {
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(express.static('public')); // Serve static files from 'public' directory
 
 // --- DATABASE MIGRATIONS ---
